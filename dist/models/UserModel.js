937387a10a98442d3a5e527e6aeff355f7ce4542
hossam <h.elshwwam123@gmail.com>
2025-06-25 11:20:40 +0300

    refactor: inline vacation balance schema into user schema

--- dist/models/UserModel.js
+++ dist/models/UserModel.js
@@ -1,11 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
-const VacationBalanceSchema = new mongoose_1.Schema({
-    sick: { type: Number, default: 0 },
-    annual: { type: Number, default: 0 },
-    casual: { type: Number, default: 0 },
-}, { _id: false });
 const userSchema = new mongoose_1.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
@@ -16,7 +11,11 @@ const userSchema = new mongoose_1.Schema({
         default: "employee",
     },
     totleLeaveDuration: { type: Number, default: 240 },
-    vacationBalance: { type: VacationBalanceSchema, required: true },
+    vacationBalance: {
+        sick: { type: Number, default: 0 },
+        annual: { type: Number, default: 0 },
+        casual: { type: Number, default: 0 },
+    },
 }, { timestamps: true });
 const UserModel = (0, mongoose_1.model)("User", userSchema);
 exports.default = UserModel;
